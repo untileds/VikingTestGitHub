6f959eac5885351748c4035bda07668d5170244b
untileds <mike0156@hotmail.com>
2016-03-30 21:49:41 +0700

    editor tools
    add more attributes

--- VikingMelody/Assets/script/tools/EditorToolScript.cs
+++ VikingMelody/Assets/script/tools/EditorToolScript.cs
@@ -6,12 +6,12 @@ using System.Xml;
 using System;
 
 public class EditorToolScript : MonoBehaviour {
-	public AudioClip sound;
-
 	private int level=0;
 	private float enemyFrequency=0f;
 	private int BPM=0;
 	private int maxSigns =0;
+	private int characterUnlock =0;
+	private string songName="";
 
 	private List<int> existingLevelList = new List<int>();
 	private Dictionary<int,int>existingLevelDic = new Dictionary<int, int>();
@@ -19,46 +19,56 @@ public class EditorToolScript : MonoBehaviour {
 	private Dictionary<int,int> existingBpmList = new Dictionary<int, int>();
 	private Dictionary<int,int> existingMaxSignsList = new Dictionary<int, int>();
 	private Dictionary<int,string> existingSongNameList = new Dictionary<int, string>();
-	// Use this for initialization
-	void Start () {
-	
+	private Dictionary<int,int> existingCharacterUnlock = new Dictionary<int, int>();
+
+	public void onSubmitCharacterUnlock(){
+		//Debug.Log( UIInput.current.value);
+		if(UIInput.current.value!=""){
+			characterUnlock =  int.Parse( UIInput.current.value);
+			Debug.Log("unLockCharacter "+characterUnlock);
+		}
+		//UIInput.current.value=null;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void onSubmitSongName(){
+		//Debug.Log( UIInput.current.value);
+		if(UIInput.current.value!=""){
+			songName = UIInput.current.value;
+	//		Debug.Log("songName "+songName);
+		}
+		//UIInput.current.value=null;
 	}
 
 	public void onSubmitLevel(){
-		Debug.Log( UIInput.current.value);
+		//Debug.Log( UIInput.current.value);
 		if(UIInput.current.value!=""){
 			level =  int.Parse( UIInput.current.value);
-			Debug.Log("level "+level);
+		//	Debug.Log("level "+level);
 		}
 		//UIInput.current.value=null;
 	}
 
 	public void onSubmitEnemyFrequency(){
-		Debug.Log( UIInput.current.value);
+	//	Debug.Log( UIInput.current.value);
 		if(UIInput.current.value!=""){
 			enemyFrequency = float.Parse(UIInput.current.value);
-			Debug.Log("enemyFrequency "+enemyFrequency);
+		//	Debug.Log("enemyFrequency "+enemyFrequency);
 			//UIInput.current.value=null;
 		}
 	}
 	public void onSubmitBPM(){
-		Debug.Log( UIInput.current.value);
+	//	Debug.Log( UIInput.current.value);
 		if(UIInput.current.value!=""){
 			BPM =int.Parse(UIInput.current.value);
-			Debug.Log("BPM "+BPM);
+		//	Debug.Log("BPM "+BPM);
 		//UIInput.current.value=null;
 		}
 	}
 	public void onSubmitMaxSigns(){
-		Debug.Log( UIInput.current.value);
+		//Debug.Log( UIInput.current.value);
 		if(UIInput.current.value!=""){
 			maxSigns =int.Parse(UIInput.current.value);
-			Debug.Log("maxSigns "+maxSigns);
+		//	Debug.Log("maxSigns "+maxSigns);
 	//	UIInput.current.value=null;
 		}
 	}
@@ -76,6 +86,7 @@ public class EditorToolScript : MonoBehaviour {
 				existingBpmList.Add(int.Parse(stageNode.Attributes["level"].Value),int.Parse(stageNode.Attributes["bpm"].Value));
 				existingMaxSignsList.Add(int.Parse(stageNode.Attributes["level"].Value),int.Parse(stageNode.Attributes["maxSigns"].Value));
 				existingSongNameList.Add(int.Parse(stageNode.Attributes["level"].Value),stageNode.Attributes["SongName"].Value);
+				existingCharacterUnlock.Add(int.Parse(stageNode.Attributes["level"].Value),int.Parse(stageNode.Attributes["unlockCharacter"].Value));
 			}
 			string xmlString ="<?xml version="+'"'+"1.0"+'"'+" encoding="+'"'+"utf-8"+'"'+"?> \n"+
 				"<!-- Level mean level of stage \n" +
@@ -93,12 +104,13 @@ public class EditorToolScript : MonoBehaviour {
 				existingEnemyFrequencyList.Remove(level);
 				existingMaxSignsList.Remove(level);
 				existingSongNameList.Remove(level);
+				existingCharacterUnlock.Remove(level);
 				existingLevelList.Add(level);
-				existingLevelDic.Add(level,level);
 				existingEnemyFrequencyList.Add(level,enemyFrequency);
 				existingBpmList.Add(level,BPM);
 				existingMaxSignsList.Add(level,maxSigns);
-				existingSongNameList.Add(level,sound.name);
+				existingSongNameList.Add(level,songName);
+				existingCharacterUnlock.Add(level,characterUnlock);
 				existingLevelList.Sort();
 //				existingEnemyFrequencyList.Sort();
 //				existingBpmList.Sort();
@@ -111,13 +123,14 @@ public class EditorToolScript : MonoBehaviour {
 				existingEnemyFrequencyList.Add(level,enemyFrequency);
 				existingBpmList.Add(level,BPM);
 				existingMaxSignsList.Add(level,maxSigns);
-				existingSongNameList.Add(level,sound.name);
+				existingSongNameList.Add(level,songName);
+				existingCharacterUnlock.Add(level,characterUnlock);
 			}
 
 			for(int i =0;i<existingLevelList.Count;i++){
 				xmlString+="<Level level = "+'"'+existingLevelList[i]+'"'+ " enemyFrequency = "+'"'+existingEnemyFrequencyList[existingLevelList[i]]+
 					'"'+" bpm = "+'"'+existingBpmList[existingLevelList[i]]+'"'+" maxSigns = "+'"'+existingMaxSignsList[existingLevelList[i]]+'"'+" SongName ="
-					+'"'+existingSongNameList[existingLevelList[i]]+'"'+ "></Level>";
+					+'"'+existingSongNameList[existingLevelList[i]]+'"'+" unlockCharacter="+'"'+existingCharacterUnlock[existingLevelList[i]]+'"'+ "></Level>";
 			}
 			xmlString+="</Stage>";
 		//	XmlDocument xmlDocEdit = new XmlDocument();
@@ -136,7 +149,7 @@ public class EditorToolScript : MonoBehaviour {
 				"-->\n" +
 				"<Stage >\n"+
 				"<Level level = "+'"'+level+'"'+ " enemyFrequency = "+'"'+enemyFrequency+'"'+" bpm = "+'"'+BPM+'"'+" maxSigns = "+'"'+maxSigns+'"'+" SongName ="
-				+'"'+sound.name+'"'+ "></Level>\n</Stage>";
+				+'"'+songName+'"'+" unlockCharacter="+'"'+characterUnlock+'"'+ "></Level>\n</Stage>";
 			XmlDocument xmldoc = new XmlDocument();
 			Debug.Log(xmlString);
 			xmldoc.LoadXml(xmlString);
